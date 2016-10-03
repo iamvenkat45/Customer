528df53e71afa0ec86d51012c02f5e450d66515b
Venkat <iamvenkat45>
2016-10-03 14:33:38 -0700

    inital commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name assignmentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the assignmentApp
+ */
+angular.module('assignmentApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
